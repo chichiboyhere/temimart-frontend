f13ae149fa24c1868cdacda07b51eba4c079973d
Tony <tonyjays@yahoo.com>
2025-01-24 20:38:40 +0100

    second commit

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -4,7 +4,7 @@ const apiClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/"
-      : "https://temimart.onrender.com",
+      : "https://temimart-backend.onrender.com",
   headers: {
     "Content-type": "application/json",
   },
