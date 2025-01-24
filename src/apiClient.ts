4f6388b4409de374a31d0288ba31618e737931dd
Tony <tonyjays@yahoo.com>
2025-01-24 20:21:47 +0100

    first commit

---
+++ src/apiClient.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/"
+      : "https://temimart.onrender.com",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    if (localStorage.getItem("userInfo"))
+      config.headers.authorization = `Bearer ${
+        JSON.parse(localStorage.getItem("userInfo")!).token
+      }`;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default apiClient;
